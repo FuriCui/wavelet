d31b6a66ecf252e62aeeebc6ff719800388547c8
FuriCui <114553431+FuriCui@users.noreply.github.com>
2023-01-28 14:59:25 -0800

    New

---
+++ StringServer.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.*;
+
+class StringServer implements URLHandler {
+    String str = new String ();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().contains("/add-message")) {
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                str += parameters[1];
+                str += "\n";
+                return str;
+            } 
+        }
+        return "404 Not Found!";
+    }  
+}
+
+class WebServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new StringServer());
+    }
+}
